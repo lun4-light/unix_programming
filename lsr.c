1a128830f9fb20a94d56928e20319be74bb42596
Jong-hyun <mo0nlight0@naver.com>
2021-11-15 00:11:44 +0900

    fix: add comment, if program get arguments but it's not a directory or it is n't exist program return error.

--- lsr.c
+++ lsr.c
@@ -8,74 +8,88 @@
 #include <string.h>
 
 void ls(char* fileName, char* nameString) {
+	/* create DIR* dp, dirent* ent */
 	DIR* dp;
 	struct dirent* dent;
 
-	if ((dp = opendir(fileName)) == NULL) {
+	strcat(nameString, fileName); 			// add filename to nameString
+
+	printf("%s:\n\n", nameString);			// print current location
+
+	if ((dp = opendir(fileName)) == NULL) {	// if opendir fail, error return
 		perror("opendir");
 		exit(1);
 	}
 
-	chdir(fileName);
+	chdir(fileName);						// change dir(filename)
 
-	chdir(filename);
-
-	while ((dent = readdir(dp))) {
-		if (strcmp(".", dent->d_name) == 0 || strcmp("..", dent->d_name) == 0)
+	while ((dent = readdir(dp))) {			// read files in directory
+		if (strcmp(".", dent->d_name) == 0 || strcmp("..", dent->d_name) == 0) 	// skip print when d_name is '.', '..'
 			continue;
 
-		printf("%s ", dent->d_name);
+		printf("%s ", dent->d_name);											// print directory name
 	}
 
 	printf("\n");
 		
-	rewinddir(dp);
+	rewinddir(dp);							// set directory pointer to front
 
-	while ((dent = readdir(dp))) {
+	while ((dent = readdir(dp))) {			// read files in directory again
 		struct stat buf;
 
-		if (strcmp(".", dent->d_name) == 0 || strcmp("..", dent->d_name) == 0)
+		if (strcmp(".", dent->d_name) == 0 || strcmp("..", dent->d_name) == 0)	// skip when d_name is '.', '..'
 			continue;
 
-		struct stat buf;
+		struct stat buf;					// file stat -> buf
 
-		stat(dent->d_name, &buf);
+		stat(dent->d_name, &buf);			// insert d_name stat -> buf
 
-		if (buf.st_mode & S_IFDIR) {
+		if (buf.st_mode & S_IFDIR) {		// if d_name is a directory
 			printf("\n\n");
-			char* before = malloc(strlen(nameString) + 4);
+			char* before = malloc(strlen(nameString) + 4);	// save nameString before ls(d_name)
 			strcpy(before, nameString);
-			strcat(nameString, "/");
+			strcat(nameString, "/");						// add nameString + '/'
 
-			ls(dent->d_name, nameString);
+			ls(dent->d_name, nameString);	// ls (d_name, nameString)
 			
-			strcpy(nameString, before);
-			free(before);
-			
+			strcpy(nameString, before);		// when finish ls, nameString must also return to its previous state.
+			free(before);					// memory free
 		}
 	}
 
-	chdir("..");
+	chdir("..");							// if function finished, change dir to before dir
 
 	closedir(dp);
 }
 
 int main(int argc, char* argv[]) {
-	char* fn;
-	char* nameString = malloc(sizeof(char) * 1000);
-	strcpy(nameString, "");
-	if (argc > 1){
-		for (int i = 1; i < argc; i++){
-			fn = argv[i];
-			ls(fn, nameString);
-			strcpy(nameString, "");
+	char* fn;										// fileName string
+	char* nameString = malloc(sizeof(char) * 1000);	// allocate memory
+	struct stat buf;								// file stat -> buf
+	strcpy(nameString, ""); 						// reset nameString
+	if (argc > 1){									// if program get arguments, repeat ls by the argument's number.
+		for (int i = 1; i < argc; i++){				
+			fn = argv[i];							// save arguments in fn
+			
+			if (stat(fn, &buf) == -1) {				// get fn's stat, if error occurs, print error and go to front of loop.
+				perror("stat fileName");			
+				continue;
+			}			
+
+			if (!(buf.st_mode & S_IFDIR)){			// if fn is not a directory
+				printf("%s\n",fn);					// print fileName
+				continue;							// go to front of loop
+			}
+
+			ls(fn, nameString); 					// run ls
+			strcpy(nameString, "");					// reset nameString
 		}
 	}
-	else {
-		fn = ".";
+	else {											// if there is no argument, do ls for the current path.
+		fn = ".";									
 		ls(fn, nameString);
 	}
 	
-	free(nameString);
+	free(nameString);								// memory free
 	return 0;
 }
