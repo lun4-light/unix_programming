3bc3b032fcffe27a07eb98c2e9bb9980f63963b1
Jong-hyun <mo0nlight0@naver.com>
2021-12-14 15:25:33 +0900

    add: final exam codes

---
+++ finals/7.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+
+int main(void) {
+    int shmid, i;
+    char* addr, addr2;
+    shmid = shmget(IPC_PRIVATE, 40, IPC_CREAT|0644);    
+
+    if (shmid == -1){
+        perror("shmget");
+        exit(1);
+    }
+
+    switch(fork()) {
+        case -1:
+            perror("fork");
+            exit(1);
+            break;
+        case 0:
+            printf("= Child begins\n");
+            addr = (char*)shmat(shmid, (char*)NULL, 0);
+            strcpy(addr, "Message from ");
+            strcat(addr, (int)getpid());
+            strcat(addr, "\n");
+            printf("= Child ends\n");
+            exit(0);
+            break;
+        default:
+            wait(0);
+            printf("= Parent begins\n");
+            addr2 = (char*)shmat(shmid, (char*)NULL, 0);
+            printf("%s",addr2);
+            shmdt((char*)addr2);
+            shmctl(shmid, IPC_RMID, (struct shmid_ds *)NULL);
+            printf("= Parent ends\n");
+            break;
+    }
+
+    return 0;
+}
